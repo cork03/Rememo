2179672d1b84867f946f860e203c4f0cf94ab4a1
cork03 <tsubasa.t1119@gmail.com>
2020-11-18 16:43:39 +0900

    create route/cards/:id(delete)

--- api/dist/src/routers/cards.js
+++ api/dist/src/routers/cards.js
@@ -29,5 +29,29 @@ route.post("/", async (req, res) => {
         res.status(400).json({ e });
     }
 });
+route.put("/:id", async (req, res) => {
+    const { card: { links, ...reqBody }, } = req.body;
+    const userId = req.user.id;
+    const { categoryIds, ...cardElements } = reqBody;
+    cardElements.userId = userId;
+    try {
+        await cards_1.default.add(cardElements, links, categoryIds);
+        res.status(201).json({});
+    }
+    catch (e) {
+        res.status(400).json({ e });
+    }
+});
+route.delete("/:id", async (req, res) => {
+    const { id } = req.params;
+    try {
+        const card = await cards_1.default.findByPk(id);
+        await card.destroy();
+        res.status(201).json({});
+    }
+    catch (e) {
+        res.status(400).json({ e });
+    }
+});
 exports.default = route;
 //# sourceMappingURL=cards.js.map
--- api/src/routers/cards.ts
+++ api/src/routers/cards.ts
@@ -32,4 +32,34 @@ route.post("/", async (req: any, res: Response) => {
   }
 });
 
+// cardの編集
+
+route.put("/:id", async (req: any, res: Response) => {
+  const {
+    card: { links, ...reqBody },
+  } = req.body;
+  const userId = req.user.id;
+  const { categoryIds, ...cardElements } = reqBody;
+  cardElements.userId = userId;
+  try {
+    await Card.add(cardElements, links, categoryIds);
+    res.status(201).json({});
+  } catch (e) {
+    res.status(400).json({ e });
+  }
+});
+
+// cardの削除
+
+route.delete("/:id", async (req: any, res: Response) => {
+  const {id} = req.params
+  try {
+    const card =  await Card.findByPk(id);
+    await card!.destroy()
+    res.status(201).json({});
+  } catch (e) {
+    res.status(400).json({ e });
+  }
+});
+
 export default route;
