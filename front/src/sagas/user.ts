2b07d111d7a86ea406bbb8a6cb8b7b26999047a1
cork03 <tsubasa.t1119@gmail.com>
2020-12-17 20:24:03 +0900

    fix login/aciton

--- front/src/sagas/user.ts
+++ front/src/sagas/user.ts
@@ -1,8 +1,17 @@
 import { call, put, takeLatest } from "redux-saga/effects";
 import * as actions from "../actions";
-import { createUser } from "../axios/user";
+import { createUser, fetchUsers } from "../axios/user";
 
-function* createUsers(action: any) {
+function* fetch(action: any) {
+  try {
+    const user = yield call(fetchUsers);
+    yield put({ type: actions.FETCH_USER_SUCCEEDED, payload: user });
+  } catch (e) {
+    yield put({ type: actions.FETCH_USER_FAILED, messagae: e.message });
+  }
+}
+
+function* create(action: any) {
   try {
     yield call(createUser, { data: action.payload });
     yield put({ type: actions.CREATE_USER_SUCCEEDED });
@@ -12,7 +21,8 @@ function* createUsers(action: any) {
 }
 
 function* user() {
-  yield takeLatest(actions.CREATE_USER_REQUESTED, createUsers);
+  yield takeLatest(actions.FETCH_USER_REQUESTED, fetch);
+  yield takeLatest(actions.CREATE_USER_REQUESTED, create);
 }
 
 export default user;
