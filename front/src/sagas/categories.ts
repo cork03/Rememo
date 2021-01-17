544822318279111ffb5519412e76f9a1db45499c
cork03 <tsubasa.t1119@gmail.com>
2021-01-17 22:50:42 +0900

    add toast

--- front/src/sagas/categories.ts
+++ front/src/sagas/categories.ts
@@ -1,6 +1,7 @@
 import { call, put, takeLatest } from "redux-saga/effects";
 import * as actions from "../actions";
 import { createCategory, fetchCategory } from "../axios/categories";
+import { errorToast, successToast } from "../utils";
 
 function* fetch(action: any) {
   try {
@@ -19,8 +20,10 @@ function* create(action: any) {
     yield call(createCategory, action.payload);
     yield put({ type: actions.CREATE_CATEGORY_SUCCEEDED });
     yield put({ type: actions.FETCH_CATEGORY_REQUESTED });
+    successToast("新しいカテゴリーを追加しました");
   } catch (e) {
     yield put({ type: actions.CREATE_CATEGORY_FAILED, messagae: e.message });
+    errorToast("カテゴリーの追加に失敗しました");
   }
 }
 
